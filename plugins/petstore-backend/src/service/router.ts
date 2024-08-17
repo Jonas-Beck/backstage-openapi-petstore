9b436c3a023ee287f7d45bb18da324f79e370a58
Jonas Beck <jona63m2@edu.campusvejle.dk>
2024-08-17 12:56:46 +0200

    feat: generate typed express router

--- plugins/petstore-backend/src/service/router.ts
+++ plugins/petstore-backend/src/service/router.ts
@@ -3,6 +3,7 @@ import { LoggerService } from '@backstage/backend-plugin-api';
 import { Config } from '@backstage/config';
 import express from 'express';
 import Router from 'express-promise-router';
+import { createOpenApiRouter } from '../schema/openapi.generated';
 
 export interface RouterOptions {
   logger: LoggerService;
@@ -13,6 +14,9 @@ export async function createRouter(
   options: RouterOptions,
 ): Promise<express.Router> {
   const { logger, config } = options;
+
+  // Only change needed for the backend router is using the createOpenApiRouter() method instead
+  // const router = createOpenApiRouter();
 
   const router = Router();
   router.use(express.json());
