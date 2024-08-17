6c111756bed93b6201113ae38571f6c2ad709973
Jonas Beck <jona63m2@edu.campusvejle.dk>
2024-08-17 13:29:31 +0200

    feat: generate API Client in common package

---
+++ plugins/petstore-common/src/generated/models/index.ts
@@ -0,0 +1,10 @@
+//
+
+export * from '../models/Address.model';
+export * from '../models/ApiResponse.model';
+export * from '../models/Category.model';
+export * from '../models/Customer.model';
+export * from '../models/Order.model';
+export * from '../models/Pet.model';
+export * from '../models/Tag.model';
+export * from '../models/User.model';
